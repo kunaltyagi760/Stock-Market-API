57aff22f3ee78ef25f00f5a4d4f489692dce5bcc
Kunal <kunaltyagi760@gmail.com>
2024-08-20 09:35:28 +0530

    email issue fixed

--- stock_investment/market/serializer.py
+++ stock_investment/market/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import AddMoney, Stock, Transaction, PurchasedStock
+from django.core.exceptions import ValidationError
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -8,6 +9,16 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'password', 'email']
+
+    def validate(self, data):
+        if 'email' not in data:
+            raise ValidationError({"email": "This field is required."})
+        return data
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("A user with that email already exists.")
+        return value
 
     def create(self, validated_data):
         user = User.objects.create_user(
